7ea35a8bfc8b250d6312954b017b7e34f1f7c857
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-09-29 17:47:25 -0500

    ftr(back): Agregando estilos a template confirmacion de registro

--- ticket-back/src/utils/email.config.js
+++ ticket-back/src/utils/email.config.js
@@ -34,11 +34,35 @@ export const sendEmail = async (email, subject, html) => {
 
 export const getTemplate = (name, date, location) => {
   return `
-          <div>
-              <span>${name} tu registro a sido exitoso</span>
-              <span>Masterclass</span>
-              <span>${location}</span>
-              <span>${date}</span>
-          </div>
+  <section
+  style="width:100%; height: 900px;"
+>
+  <div style="width: 100%; max-width: 600px; height: 900px; background: #000; padding: 30px; color: #fff; text-align: center; margin: auto;"
+  >
+    <img style="width: 300px; margin-buttom: 20px;"
+      src="https://i.ibb.co/fSSXgk6/salo-logo-full.png"
+      alt="logotipo complreto saloloyo"
+    />
+    <h2 style="font-size: 35px; margin-buttom: 20px;">
+      ${name} tu registro a sido exitoso
+    </h2>
+    <p
+      style="font-size: 30px; color: #ce4b30; font-weight: 700; font-style: italic; margin-buttom: 20px;"
+    >
+      MASTERCLASS
+    </p>
+    <p style="font-size: 20px; margin-buttom: 20px; color: #fff;">${date}</p>
+    <p style="font-size: 20px; margin-buttom: 20px; color: #fff;">${location.toUpperCase()}</p>
+    <p style="font-size: 18px; margin-buttom: 20px; color: #fff;">
+      Favor de presentarse de 10 a 15 min antes en direccion #22 col. tal
+      CCDMX la entrada sera apartir de las 12:20pm
+    </p>
+    <img
+      style="width: 300px; height: 300px; border-radius: 50%"
+      src="https://i.ibb.co/LJjBLKt/img6.jpg"
+      alt="tecladista en escenario"
+    />
+  </div>
+</section>
       `;
 };
